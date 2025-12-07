5395ec783982a1d11eda91b5294e7493f9809cab
rdPat <rdp1018ece@gmail.com>
2025-12-07 18:09:29 +0530

    commiting resilience4j circuitbreaker pattern

--- Rating Service/Rating-Service/src/main/java/com/rating/controller/RatingController.java
+++ Rating Service/Rating-Service/src/main/java/com/rating/controller/RatingController.java
@@ -2,9 +2,15 @@ package com.rating.controller;
 
 import com.rating.entity.Rating;
 import com.rating.service.RatingService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.HTML;
 import java.util.List;
 
 @RestController
@@ -33,11 +39,29 @@ public class RatingController {
     }
 
     // GET RATINGS OF A USER
+    int retryCount=1;
+
     @GetMapping("/users/{userId}")
+    @CircuitBreaker(name = "ratingHotelCircuitBreaker",fallbackMethod = "ratingHotelFallbackMethod")
+    //@Retry(name="ratingHotelRetry", fallbackMethod = "ratingHotelFallbackMethod")
     public List<Rating> getByUserId(@PathVariable Long userId) {
+        System.out.println("------------>retry count "+retryCount++);
         return ratingService.getRatingsByUserId(userId);
 
     }
+
+    //fallback method if hotel service is down
+    public List<Rating> ratingHotelFallbackMethod(Long userId, Throwable ex) {
+
+        Rating dummy = Rating.builder()
+                .ratingId("dummyID")
+                .id(0L)
+                .feedback("Hotel-Service is down — fallback response")
+                .build();
+
+        return List.of(dummy);
+    }
+
 
     // GET RATINGS OF A HOTEL
     @GetMapping("/hotels/{hotelId}")
